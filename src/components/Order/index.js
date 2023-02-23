471e672a761e45d37a279167c2c177516d363f08
arul21 <arul.21@hotmail.com>
2023-02-24 06:24:39 +0700

    add-qr

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import Header from '../Header/Header';
 import OrderItem from './OrderItem';
 import Card from '../UI/Card/Card';
+import { QRCode } from 'react-qrcode-logo';
 
 const OrderPage = () => {
   let params = useParams();
@@ -22,6 +23,20 @@ const OrderPage = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const downloadQRCode = () => {
+    // Generate download with use canvas and stream
+    const canvas = document.getElementById('qr-gen');
+    const pngUrl = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
+    let downloadLink = document.createElement('a');
+    downloadLink.href = pngUrl;
+    downloadLink.download = `${data?.invoiceId}.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   };
 
   const { itemMenus } = data;
@@ -70,6 +85,45 @@ const OrderPage = () => {
         </div>
         {itemMenus &&
           itemMenus.map((item) => <OrderItem key={item?._id} item={item} />)}
+        <div
+          style={{
+            marginTop: '25px',
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <QRCode
+            id="qr-gen"
+            value={`https://bebek-goyang.surge.sh/order/${params.id}`}
+            // qrStyle="dots"
+            // logoImage="https://paperless-menu.surge.sh/assets/logos/begos.png"
+            // logoWidth="120"
+            ecLevel="H"
+          />
+        </div>
+        <p
+          onClick={downloadQRCode}
+          style={{
+            textAlign: 'center',
+            fontSize: '12px',
+            cursor: 'pointer',
+            color: 'blue'
+          }}
+        >
+          Download QR
+        </p>
+        <div
+          style={{
+            marginTop: '25px',
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <p style={{ textAlign: 'center', fontSize: '12px' }}>
+            Anda dapat menutup aplikasi, silakan download qrcode transaksi anda
+            untuk melihat update status penyajian menu pesanan anda
+          </p>
+        </div>
         {isDone && isDone.length !== 0 && (
           <div
             onClick={() => getData()}
